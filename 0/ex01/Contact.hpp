c11da59b768917db04f921f0e07863cf0cca4d6a
Edouard <128392994+WarreDevriese@users.noreply.github.com>
2024-03-11 16:26:25 +0100

    0 ex00 and contacts for ex01

---
+++ 0/ex01/Contact.hpp
@@ -0,0 +1,33 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include <string>
+
+class Contact {
+public:
+    Contact();
+    ~Contact();
+
+    //setters
+    void setFirstName(const std::string& firstName);
+    void setLastName(const std::string& lastName);
+    void setNickname(const std::string& nickname);
+    void setPhoneNumber(const std::string& phoneNumber);
+    void setDarkestSecret(const std::string& darkestSecret);
+
+    //getters
+    std::string getFirstName() const; 
+    std::string getLastName() const;
+    std::string getNickname() const;
+    std::string getPhoneNumber() const;
+    std::string getDarkestSecret() const;
+
+private:
+    std::string firstName; 
+    std::string lastName;
+    std::string nickname;
+    std::string phoneNumber;
+    std::string darkestSecret;
+};
+
+#endif
